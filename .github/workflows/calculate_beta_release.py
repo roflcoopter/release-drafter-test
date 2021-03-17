bba2bd3ae0efd88b35de73018812a0de56e6348d
Jesper Nilsson <jespernilsson93@hotmail.se>
2021-03-17 17:53:37 +0100

    Update calculate_beta_release.py

--- .github/workflows/calculate_beta_release.py
+++ .github/workflows/calculate_beta_release.py
@@ -1,9 +1,22 @@
-import json 
-  
-f = open('response.json') 
-releases = json.load(f) 
+import json
+import re
 
-for release in releases: 
-    print(release["tag_name"]) 
+f = open("response.json")
+releases = json.load(f)
+f.close()
 
-f.close() 
+draft = releases[0]
+latest_release = releases[1]
+
+if latest_release["prerelease"]:
+    print("Latest release is a beta")
+    match = re.search(r"b(\d+)", latest_release["tag_name"])
+    if match:
+        next_beta = int(match.group(1)) + 1
+else:
+    next_beta = 1
+
+print(f"Next beta number is {next_beta}")
+
+tag_name = f"{draft['tag_name']}b{next_beta}"
+print("::set-env name=RELEASE_ID::{}".format(draft["id"]))
